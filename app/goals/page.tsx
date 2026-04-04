eae7ec64a93de212b3427a6554a1903858d2022c
Robert <128516818+rh2o6@users.noreply.github.com>
2026-04-04 15:05:45 +0000

    Goals Page

--- app/goals/page.tsx
+++ app/goals/page.tsx
@@ -2,9 +2,35 @@ export default function Goals() {
   return (
     <main>
 
-        <div className="flex items-center justify-center min-h-screen">
-            <p>Work in Progress...</p>
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold text-center mb-8">My Goals</h1>
+            
+            <section className="mb-12">
+                <h2 className="text-2xl font-semibold mb-4 text-center">Professional Goals</h2>
+             
+              <p className="text-lg mb-4">Currently, my work experience includes more infrastructure and Devops roles which I am taking a liking to.
+                However, I am still unsure of what specific career path I want to pursue in the long term. I am interested in exploring different areas of technology and gaining experience in various roles to help me make a more informed decision about my future career path.
+              </p>
+
+              <p className="text-lg"> Ideally, my next goal is to get internships in Networking, Cloud, System Adminstration, and Project/Product management to see what I like best. I am avoiding software roles for now as they are pretty generic and I feel I wont learn as much compared to the roles I mentioned. Additionally, having those skillsets would make it very easy for me to transition into software as most people probably only stick to software as it is very popular in todays age.</p>
+
+
+            </section>
+            
+            <section className="mb-12">
+                <h2 className="text-2xl font-semibold mb-4 text-center">Personal Goals</h2>
+
+            <p className="text-lg">In my personal life, I aim to improve my time management skills and maintain a better work-life balance. I also want to continue developing my communication and leadership abilities. Contiuining my volunteering in Yorks Community as well as new opportunities in my city. Additionally, I hope to purse some hobbies and intrests more like learning guitar, better educating myself on personal finances or even develop a video game one day.</p>
+
+    
+
+            </section>
+            
         </div>
+
+
+
+
       
     </main>
   )

d22c296e9827c1c9e41443c009b7391cd96e9440
Robert <128516818+rh2o6@users.noreply.github.com>
2026-04-17 14:25:52 +0000

    Cleaned up grammar and spelling

--- app/ec/page.tsx
+++ app/ec/page.tsx
@@ -1,8 +1,8 @@
 export default function Ec() {
   const experiences = [
-    { id: 1, role: "Technology Tutor", company: "Vaughan Public Libraries", date: "Nov. 2023 - Feb. 2024", desc: "Volunteered in tutoring elderly folks and program paritcipants one-on-one to learn how to use internet applications and navigate their personal devices. Additionaly informed participants of best practices and cyber security knowledge for using the internet." },
-    { id: 2, role: "Hackathon Mentor + Organizer", company: "Multiple Hackathons", date: "2025 - Present", desc: "I have organized, mentored and assisted in the orginization of multiple Hackathons such as CSHub Local Hack day, Ellehacks, & CTRL HACK DEL with initatives such as Logistics, Development and Marketing." },
-    { id: 3, role: "President", company: "The Computing Students Hub", date: "Jun. 2025 - Present", desc: "As the President of York Universities Computer Science club, I lead teams and collaborate on initatives to serve our student base such as  Hackathons, Social & Academic Events, Guest Speakers and Networking Opportunities." }
+    { id: 1, role: "Technology Tutor", company: "Vaughan Public Libraries", date: "Nov. 2023 - Feb. 2024", desc: "Volunteered in tutoring elderly folks and program participants one-on-one to learn how to use internet applications and navigate their personal devices. Additionally informed participants of best practices and cyber security knowledge for using the internet." },
+    { id: 2, role: "Hackathon Mentor + Organizer", company: "Multiple Hackathons", date: "2025 - Present", desc: "I have organized, mentored and assisted in the organization of multiple Hackathons such as CSHub Local Hack day, Ellehacks, & CTRL HACK DEL with initiatives such as Logistics, Development and Marketing." },
+    { id: 3, role: "President", company: "The Computing Students Hub", date: "Jun. 2025 - Present", desc: "As the President of York University's Computer Science club, I lead teams and collaborate on initiatives to serve our student base such as  Hackathons, Social & Academic Events, Guest Speakers and Networking Opportunities." }
   ];
 
   return (

5bdb3c046ca4930052405fb103bb8e046bc4808b
rh2o6 <roberthollander206@gmail.com>
2026-08-11 20:03:06 -0400

    Cloudlfare Issues

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  optimizeFonts: false,
+  
+ 
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+}
 
 module.exports = nextConfig
